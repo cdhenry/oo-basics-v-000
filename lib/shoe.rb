1268e604cc13738ce4842dab713a56db387e762e
Chris Henry <cdhenry@gmail.com>
2018-02-10 16:51:46 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,9 @@
 class Shoe
   attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_reader :brand
 
-  def initialize(title)
-    @title = title
-  end
-
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
+  def initialize(brand)
+    @brand = brand
   end
 end
 
